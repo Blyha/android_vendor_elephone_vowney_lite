4edcdcd8f27654552a8c4360e61682b252df1e5a
fire855 <thefire855@gmail.com>
2016-08-19 21:01:24 +0200

    Initial commit

---
+++ vowney_lite-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/vowney_lite/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/vowney_lite/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/vowney_lite/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/vowney_lite/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/vowney_lite/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/vowney_lite/vendor,system/vendor)
